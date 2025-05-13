e3da36f3c52f4f0496aa06c76ef2bbfb67f3e09b
EderCSantana <eder.cardoso.santana@gmail.com>
2025-05-13 13:47:15 +0200

    correct key added

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -33,7 +33,7 @@ const Contact = () => {
     
     try {
       // Initialize EmailJS with the correct public key
-      emailjs.init("GFqRe3DcWmHiOXw2q");
+      emailjs.init("-Q-rUQo795S9S_uXQ");
       
       // Prepare the template parameters
       const templateParams = {
